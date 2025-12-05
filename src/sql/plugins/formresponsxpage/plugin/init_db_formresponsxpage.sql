d44c4e9fc39465f25917e05368fc69f641e86788
jlf-eviden <jlf-eviden@DESKTOP-BCRT9V4>
2025-12-05 15:37:53 +0100

    LUT-31418 : Add liquibase tags to SQL scripts

--- src/sql/plugins/formresponsxpage/plugin/init_db_formresponsxpage.sql
+++ src/sql/plugins/formresponsxpage/plugin/init_db_formresponsxpage.sql
@@ -1,1 +1,4 @@
+-- liquibase formatted sql
+-- changeset core:init_db_formresponsxpage.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 insert into formresponsxpage_formsreponseedito (labelrichtext_un, labelrichtext_deux) values ('<p> test1 </p>', '<p> test2 </p>');
