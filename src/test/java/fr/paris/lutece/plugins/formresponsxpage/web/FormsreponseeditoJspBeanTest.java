19e83d95ed491f7ba6e295b88b77f7135cb3679e
jlf-eviden <jlf-eviden@DESKTOP-BCRT9V4>
2025-12-09 15:08:01 +0100

    LUT-31443 : Remove unused code
    (cherry picked from commit b2a1aa9eecbcc6549dadf63d8162b66e34fe34cb)

--- src/test/java/fr/paris/lutece/plugins/formresponsxpage/web/FormsreponseeditoJspBeanTest.java
+++ src/test/java/fr/paris/lutece/plugins/formresponsxpage/web/FormsreponseeditoJspBeanTest.java
@@ -34,20 +34,17 @@
 
 package fr.paris.lutece.plugins.formresponsxpage.web;
 
+import java.io.IOException;
+
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.mock.web.MockServletConfig;
+
 import fr.paris.lutece.portal.business.user.AdminUser;
 import fr.paris.lutece.portal.service.admin.AccessDeniedException;
 import fr.paris.lutece.portal.service.admin.AdminAuthenticationService;
-import fr.paris.lutece.portal.service.security.UserNotSignedException;
-import java.util.List;
-import java.io.IOException;
-import fr.paris.lutece.test.LuteceTestCase;
 import fr.paris.lutece.portal.service.security.SecurityTokenService;
-import fr.paris.lutece.portal.web.LocalVariables;
-import fr.paris.lutece.plugins.formresponsxpage.business.Formsreponseedito;
-import fr.paris.lutece.plugins.formresponsxpage.business.FormsreponseeditoHome;
+import fr.paris.lutece.portal.service.security.UserNotSignedException;
+import fr.paris.lutece.test.LuteceTestCase;
 /**
  * This is the business class test for the object Formsreponseedito
  */
@@ -58,116 +55,33 @@ public class FormsreponseeditoJspBeanTest extends LuteceTestCase
     private static final String LABELRICHTEXTDEUX1 = "LabelrichtextDeux1";
     private static final String LABELRICHTEXTDEUX2 = "LabelrichtextDeux2";
 
-public void testJspBeans(  ) throws AccessDeniedException, IOException
-	{	
-     	MockHttpServletRequest request = new MockHttpServletRequest();
-		MockHttpServletResponse response = new MockHttpServletResponse();
-		MockServletConfig config = new MockServletConfig();
-
-		//display admin Formsreponseedito management JSP
-		FormsreponseeditoJspBean jspbean = new FormsreponseeditoJspBean();
-		String html = jspbean.getManageFormsreponseeditos( request );
-		assertNotNull(html);
-
-		//action create Formsreponseedito
-		request = new MockHttpServletRequest();
-
-		response = new MockHttpServletResponse( );
-		AdminUser adminUser = new AdminUser( );
-		adminUser.setAccessCode( "admin" );
-		
-        
-        request.addParameter( "labelrichtext_un" , LABELRICHTEXTUN1 );
-        request.addParameter( "labelrichtext_deux" , LABELRICHTEXTDEUX1 );
-		request.addParameter("action","createFormsreponseedito");
-        request.addParameter( "token", SecurityTokenService.getInstance( ).getToken( request, "createFormsreponseedito" ));
-		request.setMethod( "POST" );
-        
-		
-		try 
-		{
-			AdminAuthenticationService.getInstance( ).registerUser(request, adminUser);
-			html = jspbean.processController( request, response ); 
-			
-			
-			// MockResponse object does not redirect, result is always null
-			assertNull( html );
-		}
-		catch (AccessDeniedException e)
-		{
-			fail("access denied");
-		}
-		catch (UserNotSignedException e) 
-		{
-			fail("user not signed in");
-		}
-
+    public void testJspBean(  ) throws AccessDeniedException, IOException
+	{				
 		//display modify Formsreponseedito JSP
-		request = new MockHttpServletRequest();
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
         request.addParameter( "labelrichtext_un" , LABELRICHTEXTUN1 );
         request.addParameter( "labelrichtext_deux" , LABELRICHTEXTDEUX1 );
-		//List<Integer> listIds = FormsreponseeditoHome.getIdFormsreponseeditosList();
-        //assertTrue( !listIds.isEmpty( ) );
-        //request.addParameter( "id", String.valueOf( listIds.get( 0 ) ) );
-		jspbean = new FormsreponseeditoJspBean();
+
+        FormsreponseeditoJspBean jspbean = new FormsreponseeditoJspBean();
 		
 		assertNotNull( jspbean.getModifyFormsreponseedito( request ) );	
 
-		//action modify Formsreponseedito
+		//action modify Formsreponseedito JSP
 		request = new MockHttpServletRequest();
-		response = new MockHttpServletResponse();
-		
-		adminUser = new AdminUser();
-		adminUser.setAccessCode("admin");
+		response = new MockHttpServletResponse();		
 		
         request.addParameter( "labelrichtext_un" , LABELRICHTEXTUN2 );
         request.addParameter( "labelrichtext_deux" , LABELRICHTEXTDEUX2 );
-		request.setRequestURI("jsp/admin/plugins/example/ManageFormsreponseeditos.jsp");
-		//important pour que MVCController sache quelle action effectuer, sinon, il redirigera vers createFormsreponseedito, qui est l'action par défaut
 		request.addParameter("action","modifyFormsreponseedito");
 		request.addParameter( "token", SecurityTokenService.getInstance( ).getToken( request, "modifyFormsreponseedito" ));
 
 		try 
 		{
+			AdminUser adminUser = new AdminUser( );
+			adminUser.setAccessCode( "admin" );
 			AdminAuthenticationService.getInstance( ).registerUser(request, adminUser);
-			html = jspbean.processController( request, response );
-
-			// MockResponse object does not redirect, result is always null
-			assertNull( html );
-		}
-		catch (AccessDeniedException e)
-		{
-			fail("access denied");
-		}
-		catch (UserNotSignedException e) 
-		{
-			fail("user not signed in");
-		}
-		
-		//get remove Formsreponseedito
-		request = new MockHttpServletRequest();
-        //request.setRequestURI("jsp/admin/plugins/example/ManageFormsreponseeditos.jsp");
-        //request.addParameter( "id", String.valueOf( listIds.get( 0 ) ) );
-		jspbean = new FormsreponseeditoJspBean();
-		request.addParameter("action","confirmRemoveFormsreponseedito");
-		assertNotNull( jspbean.getModifyFormsreponseedito( request ) );
-				
-		//do remove Formsreponseedito
-		request = new MockHttpServletRequest();
-		response = new MockHttpServletResponse();
-		request.setRequestURI("jsp/admin/plugins/example/ManageFormsreponseeditots.jsp");
-		//important pour que MVCController sache quelle action effectuer, sinon, il redirigera vers createFormsreponseedito, qui est l'action par défaut
-		request.addParameter("action","removeFormsreponseedito");
-		request.addParameter( "token", SecurityTokenService.getInstance( ).getToken( request, "removeFormsreponseedito" ));
-		//request.addParameter( "id", String.valueOf( listIds.get( 0 ) ) );
-		request.setMethod("POST");
-		adminUser = new AdminUser();
-		adminUser.setAccessCode("admin");
-
-		try 
-		{
-			AdminAuthenticationService.getInstance( ).registerUser(request, adminUser);
-			html = jspbean.processController( request, response ); 
+			String html = jspbean.processController( request, response );
 
 			// MockResponse object does not redirect, result is always null
 			assertNull( html );
@@ -180,6 +94,5 @@ public void testJspBeans(  ) throws AccessDeniedException, IOException
 		{
 			fail("user not signed in");
 		}	
-     
      }
 }
